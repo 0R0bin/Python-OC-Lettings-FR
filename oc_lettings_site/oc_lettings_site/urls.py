cc3a83cd51643b33e731a5a271041f22b88c8437
Robin <robin@azurtechresearch.fr>
2024-05-02 21:26:01 +0200

    End repartition views / templates

--- oc_lettings_site/oc_lettings_site/urls.py
+++ oc_lettings_site/oc_lettings_site/urls.py
@@ -1,13 +1,12 @@
 from django.contrib import admin
+from django.urls import include
 from django.urls import path
 
-from . import views
+from oc_lettings_site.views import index
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('lettings/', views.lettings_index, name='lettings_index'),
-    path('lettings/<int:letting_id>/', views.letting, name='letting'),
-    path('profiles/', views.profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', views.profile, name='profile'),
-    path('admin/', admin.site.urls),
+    path(r'admin/', admin.site.urls),
+    path(r'', index, name='index'),
+    path(r'lettings/', include('letting.urls')),
+    path(r'profiles/', include('profiles.urls')),
 ]
