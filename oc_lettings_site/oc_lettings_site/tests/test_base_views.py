f1597fc11f4fe985722760a4b66d77f3b9050bfc
Robin <robin@azurtechresearch.fr>
2024-05-05 01:44:15 +0200

    test exit

--- oc_lettings_site/oc_lettings_site/tests/test_base_views.py
+++ oc_lettings_site/oc_lettings_site/tests/test_base_views.py
@@ -15,7 +15,7 @@ def test_base_index_view():
     div_check = '<h1 class="page-header-ui-title mb-3 display-6">Welcome to Holiday Homes</h1>'
 
     assert div_check in content
-    assert response.status_code == 200
+    assert response.status_code == 400
     assertTemplateUsed(response, r'oc_lettings_site/index.html')
 
 
