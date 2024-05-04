b164760d3f81b96f5c6a82379ead1963b649e600
Robin <robin@azurtechresearch.fr>
2024-05-05 01:39:44 +0200

    Update settings.py

--- oc_lettings_site/oc_lettings_site/settings.py
+++ oc_lettings_site/oc_lettings_site/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-if DEBUG == False:
+if DEBUG is False:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 else:
     STATICFILES_DIRS = [
