693bdf2f7fce31525da4eed22313d8ab9e8f5c0b
Robin <robin@azurtechresearch.fr>
2024-05-05 17:39:48 +0200

    missspell

--- oc_lettings_site/oc_lettings_site/settings.py
+++ oc_lettings_site/oc_lettings_site/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['p13-oc.onrender.com.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['p13-oc.onrender.com', '127.0.0.1', 'localhost']
 CSRF_TRUSTED_ORIGINS = ['https://p13-oc.onrender.com.com',]
 
 # Application definition
